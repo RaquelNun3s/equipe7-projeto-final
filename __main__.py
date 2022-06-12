c2cf1c0b5ca6dfa9b0c336ac7c85a244c19d0171
Raquel Nunes <raquel8502@gmail.com>
2022-06-12 13:45:52 -0300

    mudancas no notebook e jobs

--- __main__.py
+++ __main__.py
@@ -13,10 +13,10 @@ blob2 = bucket.blob('job2.py')
 blob3 = bucket.blob('job3.py')
 blob4 = bucket.blob('script_inicializacao.sh')
 
-blob1.upload_from_filename(r'C:\Users\quel-\equipe7-projeto-final\Jobs\job1.py')
-blob2.upload_from_filename(r'C:\Users\quel-\equipe7-projeto-final\Jobs\job2.py')
-blob3.upload_from_filename(r'C:\Users\quel-\equipe7-projeto-final\Jobs\job3.py')
-blob4.upload_from_filename(r'C:\Users\quel-\equipe7-projeto-final\script_inicializacao.sh')
+blob1.upload_from_filename(r'C:\Users\quel-\OneDrive\Área de Trabalho\novaBranch\Jobs\job1.py')
+blob2.upload_from_filename(r'C:\Users\quel-\OneDrive\Área de Trabalho\novaBranch\Jobs\job2.py')
+blob3.upload_from_filename(r'C:\Users\quel-\OneDrive\Área de Trabalho\novaBranch\Jobs\job3.py')
+blob4.upload_from_filename(r'C:\Users\quel-\OneDrive\Área de Trabalho\novaBranch\script_inicializacao.sh')
 
 #Criando o cluster:
 cluster_name = "cluster-teste"
@@ -25,20 +25,22 @@ region = "us-east1" # Importante que seja a mesma região padrão do seu projeto
 gcs_bucket = "soulcode-mineracao"
 zone = "us-east1-c"
 
-cluster = Dataproc(cluster_name, region, project_id)#, zone)
+cluster = Dataproc(cluster_name, region, project_id, zone)
 cluster.cria_cluster()
 
 # Enviando os jobs:
 job1 = Job(region, project_id, gcs_bucket, 'job1.py', cluster_name)
 job2 = Job(region, project_id, gcs_bucket, 'job2.py', cluster_name)
-job3 = Job(region, project_id, gcs_bucket, 'job4.py', cluster_name)
+job3 = Job(region, project_id, gcs_bucket, 'job3.py', cluster_name)
 
 # try:
-#     job1.cria_job()
-#     job2.cria_job()
-#     job3.cria_job()
+#     # job1.cria_job()
+#     # job2.cria_job()
+#     # job3.cria_job()
 # except Exception as e:
 #     print(str(e))
 
 # Deletando o cluster para evitar cobranças
-cluster.deleta_cluster()
+# 
+# 
+# cluster.deleta_cluster()
