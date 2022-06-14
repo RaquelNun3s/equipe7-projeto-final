9916cc336de68cdddd6d5a5b8151c46af6c279c4
Raquel Nunes <raquel8502@gmail.com>
2022-06-14 14:01:59 -0300

    Adequações dos códigos

--- __main__.py
+++ __main__.py
@@ -5,7 +5,7 @@ from google.cloud import storage
 ServiceAccount = r'C:\Users\quel-\OneDrive\Área de Trabalho\CURSOS\Curso - Python\Projeto_final\projeto-mineracao-soulcode-85d1cc17951f.json'
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = ServiceAccount
 
-# Enviando os arquivos dos jobs para o bucket:
+# Enviando os arquivos dos jobs e do script de inicialização para o bucket:
 client = storage.Client()
 bucket = client.get_bucket('soulcode-mineracao')
 blob1 = bucket.blob('job1.py')
@@ -18,15 +18,15 @@ blob2.upload_from_filename(r'C:\Users\quel-\OneDrive\Área de Trabalho\novaBranc
 blob3.upload_from_filename(r'C:\Users\quel-\OneDrive\Área de Trabalho\novaBranch\Jobs\job3.py')
 blob4.upload_from_filename(r'C:\Users\quel-\OneDrive\Área de Trabalho\novaBranch\script_inicializacao.sh')
 
-#Criando o cluster:
+# Criando o cluster:
 cluster_name = "cluster-teste"
 project_id = "projeto-mineracao-soulcode"
-region = "us-east1" # Importante que seja a mesma região padrão do seu projeto
+region = "us-east1"
 gcs_bucket = "soulcode-mineracao"
 zone = "us-east1-c"
 
 cluster = Dataproc(cluster_name, region, project_id, zone)
-#cluster.cria_cluster()
+cluster.cria_cluster()
 
 # Enviando os jobs:
 job1 = Job(region, project_id, gcs_bucket, 'job1.py', cluster_name)
@@ -34,13 +34,11 @@ job2 = Job(region, project_id, gcs_bucket, 'job2.py', cluster_name)
 job3 = Job(region, project_id, gcs_bucket, 'job3.py', cluster_name)
 
 try:
-    # job1.cria_job()
-    # job2.cria_job()
+    job1.cria_job()
+    job2.cria_job()
     job3.cria_job()
 except Exception as e:
     print(str(e))
 
 # Deletando o cluster para evitar cobranças
-# 
-# 
-# cluster.deleta_cluster()
+cluster.deleta_cluster()
